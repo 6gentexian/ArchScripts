d1b0f2d6ecdd305e089d4778620cd2442d6788d0
6gentexian <6gentexian@gmail.com>
2016-02-29 08:49:37 -0600

    Mod'd update to break out all categories of local packages

--- update.pacman.sh
+++ update.pacman.sh
@@ -9,8 +9,23 @@
 # home dir of pacman scripts
 PACMAN_DIR="~/bin/PACMAN"
 # TMPDIR set in .bash_profile
-#################################################################################
+PACK_ALL=""
+PACK_PAC_ALL=""
+PACK_FOR_ALL=""
+# -Q
+# List all foreign packages (typically manually downloaded and installed): pacman -Qm .
+# List all native packages (installed from the sync database(s)): pacman -Qn .
+PACK_INSTALL=""
+PACK_INSTALL_PAC=""
+PACK_INSTALL_FOR=""
+# List all explicitly installed packages: pacman -Qe .
+# To list explicitly installed packages available in the official repositories: -Qen
+# To list explicitly installed packages not available in official repositories: -Qem
 
+#################################################################################
+echo $TMPDIR
+echo
+echo $R_HOME
 #################################################################################
 RESET='\e[0m'           # Text Reset
 BRed='\e[1;31m'         # Red
@@ -46,17 +61,17 @@ prGreen()
 
 # force refresh and sync all repositories
 prGreen "Refresh, sync, and update all packages"
-sudo pacman -Syyu
+#sudo pacman -Syyu
 echo; echo
 
 # installed packages not available in official repositories, -m, for foreign packages
 prYellow "All installed packages *NOT* available in official repositories "
-pacman -Qem
+#pacman -Qem
 echo; echo
 
 # creating list of all orphaned packages
 prYellow "All orphaned packages: \nPackages installed as depedencies but are now neither dependencies nor optional."
-pacman -Qdt; echo ""; echo ""
+#pacman -Qdt; echo ""; echo ""
 
 
 prGreen "Upgrade complete!!"
