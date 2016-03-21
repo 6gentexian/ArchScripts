f0f57a5d362b5c6d110d9493ecc6ccb29e8fa0bf
6gentexian <6gentexian@gmail.com>
2016-03-21 17:44:35 -0500

    Added AUR updating and dev'd a way to get pacmatic output readable

--- update.pacman.sh
+++ update.pacman.sh
@@ -8,13 +8,11 @@
 #    installed on the local system
 #
 #  TODO:  Add ability to update private repos vs ABS
-#         test for and install pacmatic
 #         add option to delete orphaned packages
 #         add AUR - paclog to check on AUR updates
 #################################################################################
-# home dir of pacman scripts
-PACMAN_USER_SCRIPT_DIR="~/bin/PACMAN"
-PACMAN_USER_CONFIG_DIR="~/.config/pacman"
+# local dir of pacman config scripts
+PACMAN_CONFIG_DIR="~/.config/pacman"
 # n.b. TMPDIR is set in .bash_profile
 DROPBOX_DIR="~/Dropbox/TW/SCRIPTS"
 
@@ -22,20 +20,18 @@ DROPBOX_DIR="~/Dropbox/TW/SCRIPTS"
 # pacman -Q   List all local packages
 # pacman -Qm  List all foreign packages (typically manually downloaded and installed)
 # pacman -Qn  List all native packages (installed from official sync'd database(s))
-PKG_ALL=$PACMAN_USER_CONFIG_DIR"/all_pkgs.txt"
-PKG_PAC_ALL=$PACMAN_USER_CONFIG_DIR"/pacman_pkgs.txt"
-PKG_FOR_ALL=$PACMAN_USER_CONFIG_DIR"/foreign_pkgs.txt"
+PKG_ALL=$PACMAN_CONFIG_DIR"/all_pkgs.txt"
+PKG_PAC_ALL=$PACMAN_CONFIG_DIR"/pacman_pkgs.txt"
+PKG_FOR_ALL=$PACMAN_CONFIG_DIR"/foreign_pkgs.txt"
 # pacman -Qe  List all explicitly installed packages
 # pacman -Qen  List explicitly installed packages available in the official repos
 # pacman -Qem  List explicitly installed packages *not* available in official repos
-PKG_INSTALL=$PACMAN_USER_CONFIG_DIR"/all_installed_pkgs.txt"
-PKG_INSTALL_PAC=$PACMAN_USER_CONFIG_DIR"/pacman_installed_pkgs.txt"
-PKG_INSTALL_FOR=$PACMAN_USER_CONFIG_DIR"/foreign_installed_pkgs.txt"
+PKG_INSTALL=$PACMAN_CONFIG_DIR"/all_installed_pkgs.txt"
+PKG_INSTALL_PAC=$PACMAN_CONFIG_DIR"/pacman_installed_pkgs.txt"
+PKG_INSTALL_FOR=$PACMAN_CONFIG_DIR"/foreign_installed_pkgs.txt"
 
 #################################################################################
-echo $TMPDIR
-echo
-echo $R_HOME
+
 #################################################################################
 RESET='\e[0m'           # Text Reset
 BRed='\e[1;31m'         # Red
@@ -68,21 +64,33 @@ prGreen()
     printf "${BGreen}$1${RESET}\n"
     echo
 }
-
+print_line() {
+  $1 "%$(tput cols)s\n"|tr ' ' '-'
+}
+print_title() {
+  clear
+  print_line $1
+  echo -e "# ${Bold}$2${Reset}"
+  print_line $1
+  echo
+}
+#################################################################################
+#
 # Refresh and sync all repositories
 prGreen "Refresh, sync, and update all repos"
-#sudo pacman -Syy
+#sudo pacmatic -Syy
 echo; echo
 
 # Update all packages
-prGreen "Update all packages ---------------------------"
+prGreen "Update all official packages ---------------------------"
 echo
 
 if [ -f /usr/bin/pacmatic ]; then
-    sudo pacmatic -Su
+    #sudo pacmatic -Su
+    echo "pacmatci installed"
 else
     while true; do
-        read -p "Would you like to use pacmatic to update (recommended)?" yn
+        read -p prYellow "Would you like to use pacmatic to update (recommended)?" yn
         case $yn in
             [Yy]* ) sudo pacman -S pacmatic; sudo pacmatic -Su; break;;
             [Nn]* ) sudo pacman -Su; exit;;
@@ -97,9 +105,14 @@ prGreen "Updating the Arch Build System local package repo"
 echo; echo
 
 # make sure that cache is cleared
-prRed "Clearing cache of tarballs..."
+prYellow "Clearing cache of tarballs..."
 # sudo pacman -Sc
 echo; echo
+
+# Update AUR packages
+prGreen "Refresh and update AUR packages"
+#packer -Syu --auronly
+echo;  echo
 
 #################################################################################
 ## Output files
@@ -111,34 +124,32 @@ prGreen "Updating the mirrorlist: /etc/pacman.d/mirrorlist"
 echo; echo
 
 # backing up list of all installed packages
-prBlue "Creating list of all installed packages in: ~/.config/pacman/all_pkgs.txt"
-# mkdir -p $PACMAN_USER_CONFIG_DIR
+prGreen "Creating list of all installed packages in: ~/.config/pacman/all_pkgs.txt"
+# mkdir -p $PACMAN_CONFIG_DIR
 # pacman -Qq > $PKG_ALL
 # mv $PKG_ALL  $DROPBOX_DIR  #~/Dropbox/TW/SCRIPTS/all_pkgs.txt
 echo; echo
 
 #  backup the current list of official repo packages
-prBlue "Creating list of all packages from the official repos: ~/.config/pacman/pacman_pkgs.txt"
+prGreen "Creating list of all packages from the official repos: ~/.config/pacman/pacman_pkgs.txt"
 # pacman -Qqn > $PKG_PAC_ALL
 # mv $PKG_PAC_ALL  $DROPBOX_DIR
 echo; echo
 
 # backup current list of packages not available in official repositories, -m, for foreign packages
-prBlue "Creating list of all foreign packages -- i.e. those *NOT* available in official repositories"
-prYellow "in:  ~/.config/pacman/foreign_pkgs.txt"
+prGreen "Creating list of all foreign packages -- i.e. those *NOT* available in official repositories in:\n  ~/.config/pacman/foreign_pkgs.txt"
 #pacman -Qqm > $PKG_FOR_ALL
 # mv $PKG_FOR_ALL  $DROPBOX_DIR
 echo; echo
 
 #  backup the current list of pacman installed packages
-prBlue "Creating list of all pacman installed packages in: ~/.config/pacman/pacman_installed_pkgs.txt"
+prGreen "Creating list of all pacman installed packages in: ~/.config/pacman/pacman_installed_pkgs.txt"
 # pacman -Qqen > $PKG_INSTALL_PAC
 # mv $PKG_INSTALL_PAC  $DROPBOX_DIR
 echo; echo
 
 # installed packages not available in official repositories, -m, for foreign packages
-prBlue "All installed foreign packages -- i.e. those *NOT* available in official repositories"
-prBlue "in:  ~/.config/pacman/foreign_installed_pkgs.txt"
+prGreen "All installed foreign packages -- i.e. those *NOT* available in official repositories in:\n  ~/.config/pacman/foreign_installed_pkgs.txt"
 #pacman -Qqem > $PKG_INSTALL_FOR
 # mv $PKG_INSTALL_FOR  $DROPBOX_DIR
 echo; echo
@@ -148,10 +159,28 @@ prRed "All orphaned packages: \nPackages installed as depedencies but are now ne
 #pacman -Qdt;
 echo; echo
 
+    while true; do
+        read -p prYellow "Would you like to remove the orphans?" yn
+        case $yn in
+            [Yy]* ) sudo pacman -S pacmatic; sudo pacmatic -Su; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+
 prRed "The following packages are optional for some installed package, but are otherwise not needed"
 (comm -13 <(pacman -Qdtq) <(pacman -Qdttq))
 echo; echo
 
+    while true; do
+        read -p prYellow "Would you like to remove the optional packages (recommended)?" yn
+        case $yn in
+            [Yy]* ) sudo pacman -S pacmatic; sudo pacmatic -Su; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
 
 #############
 # Check changelogs easily
