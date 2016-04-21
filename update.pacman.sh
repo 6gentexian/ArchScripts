fa155fb6f7cf954dad02d5af22697767617175cf
6gentexian <6gentexian@gmail.com>
2016-04-21 07:23:35 -0500

    Discovered and corrected for the fact that ANSI escape sequences are not portable - colors etc

--- update.pacman.sh
+++ update.pacman.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 #
-#  update.pacman.sh
+#  ~/bin/update.pacman.sh
 #
 #
 #  Dependencies:
 #    reflector.sh
 #
 #  This script will:
-#    Update/sync repos and packages downloaded from Arch
-#    Create files containing the names of all packages
-#    installed on the local system
+#    * Update/sync repos and packages downloaded from Arch
+#    * Create files containing the names of all local packages
 #
 #  Use:
 #    Place reflector.sh and update.pacman.sh somewhere in your $PATH, e.g. ~/bin
 #    $ ./update.pacman.sh
 #
 #  n.b.
-#    All generic functions used are in .bashrc. They are commented out at
-#      the end of this file if you don't want to place them in your startup file.
 #    $TMPDIR is set in .bash_profile or /etc/profile
 #
-#  TODO:  Add ability to update private repos vs ABS
+#  TODO:  Add ability to update private repos
 #################################################################################
 # Local dir for pacman output files
 PACMAN_CONFIG_DIR="~/.config/pacman"
-# Destination for output -- inside dropbox in this case
+
+# Destination for output files -- inside dropbox to archive
 DEST_DIR="~/Dropbox/TW/SCRIPTS"
 
 #################################################################################
@@ -42,7 +40,6 @@ PKG_FOR_ALL=$PACMAN_CONFIG_DIR"/foreign_pkgs.txt"
 PKG_INSTALL=$PACMAN_CONFIG_DIR"/all_installed_pkgs.txt"
 PKG_INSTALL_PAC=$PACMAN_CONFIG_DIR"/pacman_installed_pkgs.txt"
 PKG_INSTALL_FOR=$PACMAN_CONFIG_DIR"/foreign_installed_pkgs.txt"
-
 #################################################################################
 
 #################################################################################
@@ -52,8 +49,12 @@ BGreen='\e[1;32m'       # Green
 BYellow='\e[1;33m'      # Yellow
 BBlue='\e[1;34m'        # Blue
 #################################################################################
+
+#################################################################################
 ## To be used for updating private repos
 #UPDATE=""
+#################################################################################
+# Custom functions ----
 prYellow()
 {
     echo
@@ -78,11 +79,11 @@ prGreen()
     printf "${BGreen}$1${RESET}\n"
     echo
 }
+
 print_line() {
   $1 "%$(tput cols)s\n"|tr ' ' '-'
 }
 print_title() {
-  clear
   print_line $1
   # Move the cursor up 1 line:
   tput cuu1; tput cuu1
@@ -90,10 +91,17 @@ print_title() {
   tput cuu1
   print_line $1
 }
+
 log_to_markdown()
 {
+  # Here we have the function that will
+  #     1 load /var/log/arch-news.log
+  #     2 use awk to strip "\n" from file
+  #     3 use html2text to poop into markdown
+  #     4 view news in #MarkdownReader
+  #     5 prompt to continue
+
   if [ -f /var/log/arch-news.log ]; then
-      echo "arch"
 
       cp /var/log/arch-news.log $TMPDIR
       sudo chown $USER:users  $TMPDIR/arch-news.log
@@ -105,12 +113,12 @@ log_to_markdown()
   fi
 
   while true; do
-      echo
-      read -p "Would you like to continue?" yn
+      echo; prYellow "Would you like to continue?"
+      read -p "" yn
+
       case $yn in
           [Yy]* )
-              #sudo pacmatic -Su
-              echo "sudo pacmatic -Su"; break
+              echo "# sudo pacmatic -Su"; break
           ;;
           [Nn]* )
               prGreen "Good bye!"
@@ -120,34 +128,37 @@ log_to_markdown()
       esac
   done
 }
-#'################################################################################
+#################################################################################
+# Prep for updating
+clear
 
+# Preferably use pacmatic to update things, install if needed.
+# Otherwise use pacman
 if [ -f /usr/bin/pacmatic ]; then
     # Refresh and sync all repositories
     prGreen "Refresh, sync, and update all official repos with pacmatic"
-#    sudo pacmatic -Syy
-
-    # Here we have the function that will
-    #     1 load /var/log/arch-news.log
-    #     2 use awk to strip "\n" from file
-    #     3 use html2text to poop into markdown
-    #     4 view news in #MarkdownReader
-    #     5 prompt to continue
+    echo '# sudo pacmatic -Syy'
+exit 0
+    # Examine latest news from arch-world!
     log_to_markdown
+
+    echo "# sudo pacmatic -Su"
 else
     while true; do
-        read -p prYellow "Would you like to use pacmatic to update (recommended)?" yn
+        echo; prYellow "Would you like to use pacmatic to update (recommended)?"
+        read -p "" yn
+
         case $yn in
             [Yy]* )
-                #sudo pacman -S pacmatic
-                #sudo pacmatic -Syy
-                # log_to_markdown
-                echo "sudo pacmatic -Su"; break
+                echo '# sudo pacman -S pacmatic'
+                echo '# sudo pacmatic -Syy'
+                echo '# log_to_markdown'
+                echo "# sudo pacmatic -Su"; break
                 ;;
             [Nn]* )
                 prYellow "Updating with pacman"
-                #sudo pacman -Syy
-                #sudo pacman -Su
+                echo '# sudo pacman -Syy'
+                echo '# sudo pacman -Su'
                 ;;
             * ) echo "Please answer yes or no.";;
         esac
@@ -155,60 +166,77 @@ else
 fi
 echo; echo
 
-exit 0
 
-prGreen "Updating the Arch Build System local package repo"
-# sudo abs
-echo; echo
-
-# make sure that cache is cleared
+# Begin updates: Clear cache of tarballs -> Arch Build System -> AUR packages
+# 1. make sure that cache is cleared
 prYellow "Clearing cache of tarballs..."
-# sudo pacman -Sc
+echo '# sudo pacman -Sc'
 echo; echo
 
-# Update AUR packages
+# 2. ABS
+prGreen "Updating the Arch Build System local package repo"
+echo '# sudo abs'
+echo; echo
+
+# 3. Update the AUR packages
 prGreen "Sync AUR repos and update AUR packages"
-#packer -Syu --auronly
+echo '# packer -Syu --auronly'
 echo;  echo
 
 #################################################################################
-## Output files
-##
-# update the mirror list for pacman
-prGreen "Updating the mirrorlist: /etc/pacman.d/mirrorlist"
-# ~/bin/reflector.sh > $TMPDIR/mirrorlist
-# sudo mv $TMPDIR/mirrorlist /etc/pacman.d/mirrorlist
+# Update the mirror list for pacman and select the fastest for your locale.
+while true; do
+    echo; prYellow "Would you like to update the mirrorlist?"
+    read -p "" yn
+
+    case $yn in
+        [Yy]* )
+            prGreen "Updating the mirrorlist and choosing the fastest mirror: /etc/pacman.d/mirrorlist"
+            echo '# ~/bin/reflector.sh > $TMPDIR/mirrorlist'
+            echo '# sudo mv $TMPDIR/mirrorlist /etc/pacman.d/mirrorlist'
+            echo; break
+            ;;
+        [Nn]* )
+            prGreen "Keeping current mirrorlist!"
+            echo; break
+            ;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+# Output files to archive - classify packages in various ways and store
+# package lists in various files (in order to restore system)
+
+# Backup list of all installed packages
+prGreen "Creating list of **all** installed packages in: ~/.config/pacman/all_pkgs.txt"
+echo '# mkdir -p $PACMAN_CONFIG_DIR'
+echo '# pacman -Qq > $PKG_ALL'
+echo '# mv $PKG_ALL  $DEST_DIR'
 echo; echo
 
-# backing up list of all installed packages
-prGreen "Creating list of all installed packages in: ~/.config/pacman/all_pkgs.txt"
-# mkdir -p $PACMAN_CONFIG_DIR
-# pacman -Qq > $PKG_ALL
-# mv $PKG_ALL  $DEST_DIR  #~/Dropbox/TW/SCRIPTS/all_pkgs.txt
-echo; echo
-
-#  backup the current list of official repo packages
+# Backup list of official repo packages
 prGreen "Creating list of all packages from the official repos: ~/.config/pacman/pacman_pkgs.txt"
-# pacman -Qqn > $PKG_PAC_ALL
-# mv $PKG_PAC_ALL  $DEST_DIR
+echo '# pacman -Qqn > $PKG_PAC_ALL'
+echo '# mv $PKG_PAC_ALL  $DEST_DIR'
 echo; echo
 
-# backup current list of packages not available in official repositories, -m, for foreign packages
-prGreen "Creating list of all foreign packages -- i.e. those *NOT* available in official repositories in:\n  ~/.config/pacman/foreign_pkgs.txt"
-#pacman -Qqm > $PKG_FOR_ALL
-# mv $PKG_FOR_ALL  $DEST_DIR
+# Backup list of packages not available in official repositories, -m, for foreign packages
+prGreen "Creating list of all foreign packages -- i.e. those *NOT* from official repositories:\n  ~/.config/pacman/foreign_pkgs.txt"
+echo '# pacman -Qqm > $PKG_FOR_ALL'
+echo '# mv $PKG_FOR_ALL  $DEST_DIR'
 echo; echo
 
-#  backup the current list of pacman installed packages
-prGreen "Creating list of all pacman installed packages in: ~/.config/pacman/pacman_installed_pkgs.txt"
-# pacman -Qqen > $PKG_INSTALL_PAC
-# mv $PKG_INSTALL_PAC  $DEST_DIR
+# Backup list of pacman installed packages, not the deps of installed packages
+prGreen "Creating list of all pacman installed packages, not the deps of installed packages:\n ~/.config/pacman/pacman_installed_pkgs.txt"
+echo '# pacman -Qqen > $PKG_INSTALL_PAC'
+echo '# mv $PKG_INSTALL_PAC  $DEST_DIR'
 echo; echo
 
-# installed packages not available in official repositories, -m, for foreign packages
-prGreen "All installed foreign packages -- i.e. those *NOT* available in official repositories in:\n  ~/.config/pacman/foreign_installed_pkgs.txt"
-#pacman -Qqem > $PKG_INSTALL_FOR
-# mv $PKG_INSTALL_FOR  $DEST_DIR
+# Backup list of installed packages not available in official repositories, -m, for foreign packages
+prGreen "All installed foreign packages -- i.e. *NOT* the deps of those *NOT* from official repositories:\n  ~/.config/pacman/foreign_installed_pkgs.txt"
+echo '#pacman -Qqem > $PKG_INSTALL_FOR'
+echo '# mv $PKG_INSTALL_FOR  $DEST_DIR'
 echo; echo
 
 
@@ -220,10 +248,12 @@ pacman -Qdt;
 echo; echo
 
     while true; do
-        read -p prYellow "Would you like to remove the orphans (recommended)?" yn
+        echo; prYellow "Would you like to remove the orphans (recommended)?"
+        read -p "" yn
+
         case $yn in
             [Yy]* )
-                sudo pacman -Rns $(pacman -Qtdq); break
+                echo 'sudo pacman -Rns $(pacman -Qtdq)'; break
                 ;;
             [Nn]* )
                 prGreen "Orphans left alone"; break
@@ -238,10 +268,12 @@ prRed "The following packages are optional for some installed package, but are o
 echo; echo
 
     while true; do
-        read -p prYellow "Would you like to remove the optional packages?" yn
+        echo; prYellow "Would you like to remove the optional packages?"
+        read -p "" yn
+
         case $yn in
             [Yy]* )
-                sudo pacman -Rns $(comm -13 <(pacman -Qdtq) <(pacman -Qdttq)); break
+                echo 'sudo pacman -Rns $(comm -13 <(pacman -Qdtq) <(pacman -Qdttq))'; break
                 ;;
             [Nn]* )
                 prGreen "Optional packages left alone"; break
@@ -271,49 +303,4 @@ print_title prGreen "Sync/Upgrade complete!!"
 #        file into the / directory and executing the following command:
 #
 # $ sudo tar -xjvf pacman_database.tar.bz2
-################################################################################
-
-
-# #################################################################################
-# RESET='\e[0m'           # Text Reset
-# BRed='\e[1;31m'         # Red
-# BGreen='\e[1;32m'       # Green
-# BYellow='\e[1;33m'      # Yellow
-# BBlue='\e[1;34m'        # Blue
-# #################################################################################
-# ## To be used for updating private repos
-# #UPDATE=""
-# prYellow()
-# {
-#     echo
-#     printf "${BYellow}$1${RESET}\n"
-#     echo
-# }
-# prRed()
-# {
-#     echo
-#     printf "${BRed}$1${RESET}\n"
-#     echo
-# }
-# prBlue()
-# {
-#     echo
-#     printf "${BBlue}$1${RESET}\n"
-#     echo
-# }
-# prGreen()
-# {
-#     echo
-#     printf "${BGreen}$1${RESET}\n"
-#     echo
-# }
-# print_line() {
-#   $1 "%$(tput cols)s\n"|tr ' ' '-'
-# }
-# print_title() {
-#   clear
-#   print_line $1
-#   echo -e "# ${Bold}$2${Reset}"
-#   print_line $1
-#   echo
-# }
+#################################################################################
