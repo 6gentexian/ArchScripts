8ba9563cef9d091999ae94fd91bed2de0e050580
6gentexian <6gentexian@gmail.com>
2016-04-22 02:38:36 -0500

    Rm'd Xauthority and finished updateGS

--- update.pacman.sh
+++ update.pacman.sh
@@ -20,10 +20,10 @@
 #  TODO:  Add ability to update private repos
 #################################################################################
 # Local dir for pacman output files
-PACMAN_CONFIG_DIR="~/.config/pacman"
+PACMAN_CONFIG_DIR="$HOME/.config/pacman"
 
 # Destination for output files -- inside dropbox to archive
-DEST_DIR="~/Dropbox/TW/SCRIPTS"
+DEST_DIR="$HOME/Dropbox/TW/SCRIPTS"
 
 #################################################################################
 # Output file names
@@ -100,7 +100,6 @@ log_to_markdown()
   #     3 use html2text to poop into markdown
   #     4 view news in #MarkdownReader
   #     5 prompt to continue
-
   if [ -f /var/log/arch-news.log ]; then
 
       cp /var/log/arch-news.log $TMPDIR
@@ -118,7 +117,7 @@ log_to_markdown()
 
       case $yn in
           [Yy]* )
-              echo "# sudo pacmatic -Su"; break
+              break
           ;;
           [Nn]* )
               prGreen "Good bye!"
@@ -132,57 +131,6 @@ log_to_markdown()
 # Prep for updating
 clear
 
-# Preferably use pacmatic to update things, install if needed.
-# Otherwise use pacman
-if [ -f /usr/bin/pacmatic ]; then
-    # Refresh and sync all repositories
-    prGreen "Refresh, sync, and update all official repos with pacmatic"
-    echo '# sudo pacmatic -Syy'
-exit 0
-    # Examine latest news from arch-world!
-    log_to_markdown
-
-    echo "# sudo pacmatic -Su"
-else
-    while true; do
-        echo; prYellow "Would you like to use pacmatic to update (recommended)?"
-        read -p "" yn
-
-        case $yn in
-            [Yy]* )
-                echo '# sudo pacman -S pacmatic'
-                echo '# sudo pacmatic -Syy'
-                echo '# log_to_markdown'
-                echo "# sudo pacmatic -Su"; break
-                ;;
-            [Nn]* )
-                prYellow "Updating with pacman"
-                echo '# sudo pacman -Syy'
-                echo '# sudo pacman -Su'
-                ;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-fi
-echo; echo
-
-
-# Begin updates: Clear cache of tarballs -> Arch Build System -> AUR packages
-# 1. make sure that cache is cleared
-prYellow "Clearing cache of tarballs..."
-echo '# sudo pacman -Sc'
-echo; echo
-
-# 2. ABS
-prGreen "Updating the Arch Build System local package repo"
-echo '# sudo abs'
-echo; echo
-
-# 3. Update the AUR packages
-prGreen "Sync AUR repos and update AUR packages"
-echo '# packer -Syu --auronly'
-echo;  echo
-
 #################################################################################
 # Update the mirror list for pacman and select the fastest for your locale.
 while true; do
@@ -192,8 +140,8 @@ while true; do
     case $yn in
         [Yy]* )
             prGreen "Updating the mirrorlist and choosing the fastest mirror: /etc/pacman.d/mirrorlist"
-            echo '# ~/bin/reflector.sh > $TMPDIR/mirrorlist'
-            echo '# sudo mv $TMPDIR/mirrorlist /etc/pacman.d/mirrorlist'
+            ~/bin/reflector.sh > $TMPDIR/mirrorlist
+            sudo mv $TMPDIR/mirrorlist /etc/pacman.d/mirrorlist
             echo; break
             ;;
         [Nn]* )
@@ -203,40 +151,91 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+#################################################################################
+
+# Begin updates: Clear cache of tarballs -> UP pacman -> UP ABS -> UP AUR packages
+# 1. make sure that cache is cleared
+prYellow "Clearing cache of tarballs..."
+sudo pacman -Sc
+echo; echo
+
+# Preferably use pacmatic to update things, install if needed.
+# Otherwise use pacman
+if [ -f /usr/bin/pacmatic ]; then
+    # Refresh and sync all repositories
+    prGreen "Refresh, sync, and update all official repos with pacmatic"
+    sudo pacmatic -Syu
+
+    # Examine latest news from arch-world!
+    log_to_markdown
+else
+    while true; do
+        echo; prYellow "Would you like to use pacmatic to update (recommended)?"
+        read -p "" yn
+
+        case $yn in
+            [Yy]* )
+                sudo pacman -S pacmatic
+                sudo pacmatic -Syy
+                log_to_markdown
+                sudo pacmatic -Su; break
+                ;;
+            [Nn]* )
+                prYellow "Updating with pacman"
+                sudo pacman -Syy
+                sudo pacman -Su; break
+                ;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+echo; echo
 
 
+# 3. ABS
+prGreen "Updating the Arch Build System local package repo"
+sudo abs
+echo; echo
+
+# 4. Update the AUR packages
+prGreen "Sync AUR repos and update AUR packages"
+packer -Syu --auronly
+echo;  echo
+
+
+#################################################################################
 # Output files to archive - classify packages in various ways and store
 # package lists in various files (in order to restore system)
 
-# Backup list of all installed packages
-prGreen "Creating list of **all** installed packages in: ~/.config/pacman/all_pkgs.txt"
-echo '# mkdir -p $PACMAN_CONFIG_DIR'
-echo '# pacman -Qq > $PKG_ALL'
-echo '# mv $PKG_ALL  $DEST_DIR'
+# Backup list of all local packages on this machine
+prGreen "Creating list of **all** packages on $HOSTNAME: ~/.config/pacman/all_pkgs.txt"
+mkdir -p $PACMAN_CONFIG_DIR
+pacman -Qq > $PKG_ALL
+mv $PKG_ALL  $DEST_DIR
 echo; echo
 
 # Backup list of official repo packages
-prGreen "Creating list of all packages from the official repos: ~/.config/pacman/pacman_pkgs.txt"
-echo '# pacman -Qqn > $PKG_PAC_ALL'
-echo '# mv $PKG_PAC_ALL  $DEST_DIR'
+prGreen "Creating list of packages from the official repos: ~/.config/pacman/pacman_pkgs.txt"
+pacman -Qqn > $PKG_PAC_ALL
+mv $PKG_PAC_ALL  $DEST_DIR
 echo; echo
 
 # Backup list of packages not available in official repositories, -m, for foreign packages
-prGreen "Creating list of all foreign packages -- i.e. those *NOT* from official repositories:\n  ~/.config/pacman/foreign_pkgs.txt"
-echo '# pacman -Qqm > $PKG_FOR_ALL'
-echo '# mv $PKG_FOR_ALL  $DEST_DIR'
+prGreen "Creating list of foreign packages -- i.e. those *NOT* from official repositories:\n  ~/.config/pacman/foreign_pkgs.txt"
+pacman -Qqm > $PKG_FOR_ALL
+mv $PKG_FOR_ALL  $DEST_DIR
 echo; echo
 
 # Backup list of pacman installed packages, not the deps of installed packages
-prGreen "Creating list of all pacman installed packages, not the deps of installed packages:\n ~/.config/pacman/pacman_installed_pkgs.txt"
-echo '# pacman -Qqen > $PKG_INSTALL_PAC'
-echo '# mv $PKG_INSTALL_PAC  $DEST_DIR'
+prGreen "Creating list of pacman installed packages, not the deps of installed packages:\n ~/.config/pacman/pacman_installed_pkgs.txt"
+pacman -Qqen > $PKG_INSTALL_PAC
+mv $PKG_INSTALL_PAC  $DEST_DIR
 echo; echo
 
 # Backup list of installed packages not available in official repositories, -m, for foreign packages
-prGreen "All installed foreign packages -- i.e. *NOT* the deps of those *NOT* from official repositories:\n  ~/.config/pacman/foreign_installed_pkgs.txt"
-echo '#pacman -Qqem > $PKG_INSTALL_FOR'
-echo '# mv $PKG_INSTALL_FOR  $DEST_DIR'
+prGreen "Creating list of installed foreign packages -- i.e. *NOT* the deps of foreign packages:\n  ~/.config/pacman/foreign_installed_pkgs.txt"
+pacman -Qqem > $PKG_INSTALL_FOR
+mv $PKG_INSTALL_FOR  $DEST_DIR
 echo; echo
 
 
@@ -253,7 +252,7 @@ echo; echo
 
         case $yn in
             [Yy]* )
-                echo 'sudo pacman -Rns $(pacman -Qtdq)'; break
+                sudo pacman -Rns $(pacman -Qtdq); break
                 ;;
             [Nn]* )
                 prGreen "Orphans left alone"; break
@@ -273,7 +272,7 @@ echo; echo
 
         case $yn in
             [Yy]* )
-                echo 'sudo pacman -Rns $(comm -13 <(pacman -Qdtq) <(pacman -Qdttq))'; break
+                sudo pacman -Rns $(comm -13 <(pacman -Qdtq) <(pacman -Qdttq)); break
                 ;;
             [Nn]* )
                 prGreen "Optional packages left alone"; break
@@ -282,25 +281,26 @@ echo; echo
         esac
     done
 
+
 ################################################################################
 print_title prGreen "Sync/Upgrade complete!!"
 #################################################################################
 
 
+
+
 #################################################################################
-# Other useful things
+# Other useful things:  Back-up the pacman database -----------------------------
 #
-#
-#
-# Back-up the pacman database ---------------------------------------------------
 #
 # The following command can be used to back up the local pacman database:
 #
 # $ tar -cjf pacman_database.tar.bz2 /var/lib/pacman/local
 #
 #
-# n.b. The database can be restored by moving the pacman_database.tar.bz2
-#        file into the / directory and executing the following command:
+# n.b.
+#     The database can be restored by moving the pacman_database.tar.bz2
+#     file into the / directory and executing the following command:
 #
 # $ sudo tar -xjvf pacman_database.tar.bz2
 #################################################################################
